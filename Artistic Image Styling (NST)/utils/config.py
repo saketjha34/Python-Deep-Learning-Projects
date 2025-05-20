13fcb3946825a1acfae4f69a8fa6b2c3f41385ae
Saket Jha <saketjha0324@gmail.com>
2025-05-20 14:47:24 +0530

    Update config.py

--- Artistic Image Styling (NST)/utils/config.py
+++ Artistic Image Styling (NST)/utils/config.py
@@ -1,49 +1,25 @@
-import os
-import torch
-import torch.nn as nn
 from torchvision.transforms import transforms , v2
 
-def get_default_device():
-    """Pick GPU if available, else CPU"""
-    if torch.cuda.is_available():
-        return torch.device('cuda')
-    else:
-        return torch.device('cpu')
-    
+import yaml
 
-def to_device(data, device):
-    """Move tensor(s) to chosen device"""
-    if isinstance(data, (list,tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
+class StyleTransferConfig:
+    def __init__(self, config_path='config.yaml'):
+        with open(config_path, 'r') as f:
+            cfg = yaml.safe_load(f)
 
+        self.EPOCHS = cfg['EPOCHS']
+        self.LEARNING_RATE = cfg['LEARNING_RATE']
+        self.WEIGHTS = tuple(cfg['WEIGHTS'])
+        self.IMG_SIZE = cfg['IMG_SIZE']
+        self.IMG_CHANNEL = cfg['IMG_CHANNEL']
+        self.IMG_NAME = cfg['IMG_NAME']
+        self.ORIGINAL_IMG_PATH = cfg['ORIGINAL_IMG_PATH']
+        self.STYLE_IMG_PATH = cfg['STYLE_IMG_PATH']
 
-class DeviceDataLoader():
-    """Wrap a dataloader to move data to a device"""
-    def __init__(self, dl, device):
-        self.dl = dl
-        self.device = device
-        
-    def __iter__(self):
-        """Yield a batch of data after moving it to device"""
-        for b in self.dl: 
-            yield to_device(b, self.device)
+StyleTransferconfig = StyleTransferConfig()
 
-    def __len__(self):
-        """Number of batches"""
-        return len(self.dl)
-    
-EPOCHS = 4000
-LEARNING_RATE = 1e-3
-WEIGHTS = (2,0.01)
-IMG_SIZE = 720
-IMG_CHANNEL = 3
-ORIGINAL_IMG_PATH = 'path/to/your/input/image.jpg'
-STYLE_IMG_PATH = 'path/to/styling/image.jpg'
-    
 image_transforms = transforms.Compose([
-        v2.Resize(size = (IMG_SIZE,IMG_SIZE)),
-        v2.CenterCrop((IMG_SIZE,IMG_SIZE)),
+        v2.Resize(size = (StyleTransferconfig.IMG_SIZE,StyleTransferconfig.IMG_SIZE)),
+        v2.CenterCrop((StyleTransferconfig.IMG_SIZE,StyleTransferconfig.IMG_SIZE)),
         transforms.ToTensor(),
     ])
-
